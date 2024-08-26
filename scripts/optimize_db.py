451a06a19b8e89ab71280dfbbf08300d506fe422
Command <me@c0mm4nd.com>
2024-08-26 18:29:37 +0800

    bugfix: Prevent the last failed session from being locked and subsequent commands cannot be executed

--- scripts/optimize_db.py
+++ scripts/optimize_db.py
@@ -2,24 +2,30 @@ import clickhouse_connect
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--db', type=str, required=True)
-parser.add_argument('--dsn', type=str, required=True)
-parser.add_argument('--timeout', type=int, default=600)
+parser.add_argument("--db", type=str, required=True)
+parser.add_argument("--dsn", type=str, required=True)
+parser.add_argument("--timeout", type=int, default=600)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parser.parse_args()
-    client = clickhouse_connect.get_client(
-        dsn=args.dsn,
-        send_receive_timeout=args.timeout,
-    )
-    
+
     try:
-        result = client.query('SHOW TABLES FROM {}'.format(args.db))
+        client = clickhouse_connect.get_client(
+            dsn=args.dsn,
+            send_receive_timeout=args.timeout,
+        )
+        result = client.query("SHOW TABLES FROM {}".format(args.db))
         for table in result.result_rows:
-            print('Optimizing table {}.{}'.format(args.db, table[0]))
+            print("Optimizing table {}.{}".format(args.db, table[0]))
             try:
-                client.command('OPTIMIZE TABLE {}.{} DEDUPLICATE'.format(args.db, table[0]))
+                client = clickhouse_connect.get_client(
+                    dsn=args.dsn,
+                    send_receive_timeout=args.timeout,
+                )
+                client.command(
+                    "OPTIMIZE TABLE {}.{} FINAL DEDUPLICATE".format(args.db, table[0])
+                )
             except Exception as e:
-                print('Error optimizing table {}.{}: {}'.format(args.db, table[0], e))
+                print("Error optimizing table {}.{}: {}".format(args.db, table[0], e))
     except Exception as e:
-        print('Error getting tables from {}: {}'.format(args.db, e))
+        print("Error getting tables from {}: {}".format(args.db, e))
